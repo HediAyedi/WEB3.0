01d409973e4c8082162d303617f371f47b15ad7d
Hedi Ayedi <hayedi@ballsquad.pl>
2025-09-03 16:03:43 +0200

    updateé

--- src/components/Transactions.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Transacions() {
-    return (
-        <h1>Transacions</h1>
-    );
-}
-
-export default Transacions;
--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Loader } from './Loader';
-export { default as Transacions } from './Transactions';
-export { default as Services } from './Services';
-export { default as Welcome } from './Welcome';
-export { default as Footer } from './Footer';
-
